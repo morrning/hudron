03d9a21812ff1c7a5d61deae8ff629bfb177b6ac
Babak Alizadeh <alizadeh.babak@gmail.com>
2022-12-30 14:22:17 -0500

    some work on login and logout part

--- core/src/Form/RegistrationFormType.php
+++ core/src/Form/RegistrationFormType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,10 +20,10 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('codemeli')
+            ->add('email',EmailType::class)
+            ->add('codemeli',NumberType::class,['html5'=> true, 'attr'=>['min'=>1000000000]])
             ->add('birthday')
-            ->add('mobile')
+            ->add('mobile',TelType::class)
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
